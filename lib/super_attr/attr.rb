f950b67b4970e8e837e878daafabb8d6694ea742
Hugo Miranda Mac <hugo@aioninnovations.com>
2015-09-23 15:10:42 -0700

    add init_super_attrs method to AttrBase

--- lib/super_attr/attr.rb
+++ lib/super_attr/attr.rb
@@ -6,12 +6,19 @@ module SuperAttr
   	end
 
 		module AttrBase
+
+			@@required_attrs = []
+
+			# super_attr accpects the attr name and a hash
+			# with type: and required:
 			def super_attr(name, opts={})
 				
+				# Defines getter
 				define_method("#{name}") do
 					instance_variable_get("@#{name}")
 				end
 
+				# Defines setter
 				define_method("#{name}=") do |arg|
 					if arg.is_a? opts[:type]
 						instance_variable_set("@#{name}", arg)
@@ -19,8 +26,35 @@ module SuperAttr
 						raise StandardError.new("The value for #{name} is not a type #{opts[:type]}")
 					end
 				end
-				
+
+				@@required_attrs << name if opts.has_key?(:required) && opts[:required]
+
 			end
+
+			def init_super_attrs(args={}, inst)
+				args_keys = args.keys
+
+				if @@required_attrs.any?
+					# Checks if args_keys has all attr in the 
+					# @@required_attrs. If args is empty, it will
+					# raise an StandardError
+					if args_keys.any?
+						not_set_attrs = []
+						@@required_attrs.each do |req|
+							not_set_attrs << req unless args_keys.include?(req)
+						end
+
+						raise StandardError.new("Required attributes: #{not_set_attrs.join(", ")}") if not_set_attrs.any?
+					else
+						raise StandardError.new("Required attributes: #{@@required_attrs.join(", ")}")
+					end
+
+				end
+				
+				# Inits the args
+				args_keys.each {|key| self.instance_method("#{key}=").bind(inst).call(args[key]) }
+			end
+
 		end
 
 	end
--- test/super_attr_test.rb
+++ test/super_attr_test.rb
@@ -6,17 +6,36 @@ describe "SuperAttr:Attr" do
 	class Thing
 		include SuperAttr::Attr
 
-		super_attr :my_int, type: Integer, required: true 
+		super_attr :my_int, type: Integer, required: true
+		super_attr :my_string, type: String, required: true
+		super_attr :my_hash, type: Hash
 
 		def initialize(args={})
-			@my_int = args[:my_int]
+			Thing.init_super_attrs(args, self)
 		end
 
 	end
 
-	it "set the wrong type for attr" do
-		err = -> { Thing.new(my_int: "foo") }.must_raise StandardError
+	it "inits without any required attribute" do
+		err = -> { Thing.new() }.must_raise StandardError
+		err.message.must_equal 'Required attributes: my_int, my_string'
+	end
+
+	it "inits without required attribute :my_int" do
+		err = -> { Thing.new(my_string: "foo") }.must_raise StandardError
+		err.message.must_equal 'Required attributes: my_int'
+	end
+
+	it "sets the wrong type for attribute" do
+		err = -> { Thing.new(my_int: "foo", my_string: 20, my_hash: "foo") }.must_raise StandardError
 		err.message.must_equal 'The value for my_int is not a type Integer'
+	end
+
+	it "has the right types" do
+		t = Thing.new(my_int: 20, my_string: "foo", my_hash: {name: "foo"})
+		t.my_int.must_be_kind_of Integer
+		t.my_string.must_be_kind_of String
+		t.my_hash.must_be_kind_of Hash
 	end
 
 end
